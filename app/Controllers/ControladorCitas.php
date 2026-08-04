6b05c90856d4afb0ce75a1555a0a593ce8c4b674
Juan Arévalo <juancarlosa1035@gmail.com>
2026-08-03 23:16:19 -0600

    Nuevos archivos agregados

---
+++ app/Controllers/ControladorCitas.php
@@ -0,0 +1,35 @@
+<?php 
+
+class ControladorCitas{
+
+    Public function inico()
+    {
+
+    }
+
+    public function registrar()
+    {
+
+    }
+
+    public function guardar()
+    {
+
+    }
+
+    public function listar()
+    {
+
+    }
+
+    public function editar()
+    {
+
+    }
+
+    public function eliminar()
+    {
+
+    }
+
+}
--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
 
-echo "<h1>Sistema de Gestión de Citas</h1>";
+require_once 'app/Controllers/ControladorCitas.php';
+
+$controlador = new ControladorCitas();
+
+$accion = $_GET['accion'] ?? 'inicio';
+
+switch ($accion) {
+
+    case 'inicio':
+        $controlador->inicio();
+        break;
+
+    case 'registrar':
+        $controlador->registrar();
+        break;
+
+    case 'listar':
+        $controlador->listar();
+        break;
+
+    case 'editar':
+        $controlador->editar();
+        break;
+
+    default:
+        require 'app/Views/error404.php';
+        break;
+}
