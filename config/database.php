bd2419237741f8c6a3dae475c57f6273f9ec0748
sammy1799 <samuelramirez789@gmail.com>
2026-08-08 21:17:38 -0600

    Sincronizando archivo database.php exitosamente

---
+++ config/database.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class Database {
+    public function conectar() {
+        $host = "localhost";
+        $usuario = "root";
+        $password = "";
+        $base_datos = "sistema_gestion_citas";
+
+
+        try {
+            $conexion = new PDO(
+                "mysql:host=$host;dbname=$base_datos;charset=utf8mb4",
+                $usuario,
+                $password
+            );
+
+            $conexion->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+
+            return $conexion;
+
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+}
+
+?>
